c3127e770bcf890045dbf2171519cd8e89325a60
Arianna Avalos <68700321+ariavalosm@users.noreply.github.com>
2022-06-08 15:56:03 -0500

    botones

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import './App.css';
+import "./hoja-de-estilo/Boton.css";
+import Boton from './componentes/Boton';
 import freecodecamplogo from "./imagenes/freecodecamplogo.png";
 
 function App() {
+
+  const manejarClic = () => {
+    console.log("Clic");
+  }
+
+  const reiniciarContador = () => {
+    console.log("Reiniciar");
+  }
+
   return (
     <div className="App">
       <div className="freecodecamp-logo-contenedor">
@@ -11,6 +22,14 @@ function App() {
         alt="Logo de freecodecamp"/>
       </div>
       <div className="contenedor-principal">
+        <Boton
+          texto= "Clic"
+          esBotonDeClic={true}
+          manejarClic={manejarClic}/>
+        <Boton
+          texto="Reiniciar"
+          esBotonDeClic={false}
+          manejarClic={reiniciarContador}/>
 
       </div>
     </div>
